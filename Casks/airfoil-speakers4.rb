036c8551d875bc49411f3a22cae89cf785c33354
John Randall <john@johnrandall.com>
2016-06-24 18:21:00 -0400

    add airfoil-speakers4

---
+++ Casks/airfoil-speakers4.rb
@@ -0,0 +1,11 @@
+cask 'airfoil-speakers4' do
+  version '4.9.2'
+  sha256 'ee30619d91ec88fe7a64c1c8a098c619aad850595fbb8d7f23f8b8ba8a1ceb77'
+
+  url 'https://www.rogueamoeba.com/legacy/downloads/AirfoilSpeakers-492.zip'
+  name 'Airfoil Speakers'
+  homepage 'https://www.rogueamoeba.com/legacy/'
+  license :commercial
+
+  app 'Airfoil Speakers.app'
+end
